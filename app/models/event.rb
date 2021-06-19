bd962510cd508c93633f0e6c68f993613ec2e314
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-06-19 22:56:24 +0900

    Event model validates
    refs #5

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :place, presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 2000 }
+  validates :start_at, presence: true
+  validates :end_at, presence: true
+  validates :start_at_before_end_at
+
+
+  private
+
+  def start_at_before_end_at
+    return unless start_at && end_at
+
+    errors.add(:start_at, 'は終了時間よりも前に設定してください') if start_at >= end_at
+  end
 end
