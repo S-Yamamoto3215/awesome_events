02b69ffb9f6f2f65023f7bcacba34b1007863cd3
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-06-25 23:44:22 +0900

    TicketsController new、createの実装、i18nの編集
    refs #14

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,15 @@
 class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingError
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+
+    redirect_to event, notice: 'このイベントに参加登録しました' if @ticket.save
+  end
 end
