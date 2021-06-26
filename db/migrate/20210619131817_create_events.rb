2a9102d6e81f6bbb05ed38802fbb8859221ad771
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-06-26 16:14:02 +0900

    DBの設定変更、eventsテーブルのowner_idにnullを許容
    ref #16

--- db/migrate/20210619131817_create_events.rb
+++ db/migrate/20210619131817_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-      t.bigint :owner_id,   null: false
+      t.bigint :owner_id
       t.string :name,       null: false
       t.string :place,      null: false
       t.datetime :start_at, null: false
