092509f7297bb138289b5888ff7e37ec5fc92e37
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-07-05 23:53:32 +0900

    モックを使ったテストに書き換え
    refs #25

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,19 +3,12 @@ require "test_helper"
 class EventTest < ActiveSupport::TestCase
   test 'Event.created_by?' do
     # created_by? => イベント作成者本人かどうか
-    # event作成
     event = FactoryBot.create(:event)
+    owner_user = MiniTest::Mock.new.expect(:id, event.owner_id)
+    other_user = MiniTest::Mock.new.expect(:id, 2)
 
-    user = User.new
-    user.stub(:id, event.owner_id) do
-      assert_equal(true, event.created_by?(user))
-    end
-    #
-    # # created_by?の引数が作成者と同じ
-    #
-    # # created_by?の引数が作成者と異なる
-    # assert_equal(false, target_event.created_by?(other_user))
-    # # created_by?の引数がnil
-    # assert_equal(false, target_event.created_by?(nil))
+    assert_equal(true, event.created_by?(owner_user))
+    assert_equal(false, event.created_by?(other_user))
+    assert_equal(false, event.created_by?(nil))
   end
 end
