0667b1d309e3cc85f86134fcfe26dc18269615e3
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-07-05 23:40:55 +0900

    スタブを使ったテスト
    refs #25

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,18 +3,19 @@ require "test_helper"
 class EventTest < ActiveSupport::TestCase
   test 'Event.created_by?' do
     # created_by? => イベント作成者本人かどうか
-    # userを2種類用意
-    owner_user = FactoryBot.create(:user)
-    other_user = FactoryBot.create(:user)
+    # event作成
+    event = FactoryBot.create(:event)
 
-    # owner_userでevent作成
-    target_event = FactoryBot.create(:event, owner: owner_user)
-
-    # created_by?の引数が作成者と同じ
-    assert_equal(true, target_event.created_by?(owner_user))
-    # created_by?の引数が作成者と異なる
-    assert_equal(false, target_event.created_by?(other_user))
-    # created_by?の引数がnil
-    assert_equal(false, target_event.created_by?(nil))
+    user = User.new
+    user.stub(:id, event.owner_id) do
+      assert_equal(true, event.created_by?(user))
+    end
+    #
+    # # created_by?の引数が作成者と同じ
+    #
+    # # created_by?の引数が作成者と異なる
+    # assert_equal(false, target_event.created_by?(other_user))
+    # # created_by?の引数がnil
+    # assert_equal(false, target_event.created_by?(nil))
   end
 end
