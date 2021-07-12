4229a37e1df95900feb643b6664b4d63eccfc3be
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2021-07-12 23:29:59 +0900

    イベント登録に関するテスト修正

--- test/system/events_test.rb
+++ test/system/events_test.rb
@@ -23,24 +23,13 @@ class EventsTest < ApplicationSystemTestCase
 
     fill_in '名前', with: 'Miyazaki.rb'
     fill_in '場所', with: '宮崎'
-    fill_in '内容', with: '宮崎のRubyエンジニアコミュニティ'
 
     start_at = Time.current
     end_at = start_at + 3.hour
+    fill_in '開始時間', with: start_at
+    fill_in '終了時間', with: end_at
 
-    start_at_field = 'event_start_at'
-    select start_at.strftime('%Y'), from: "#{start_at_field}_1i"
-    select I18n.l(start_at, format: '%B'), from: "#{start_at_field}_2i"
-    select start_at.strftime('%-d'), from: "#{start_at_field}_3i"
-    select start_at.strftime('%H'), from: "#{start_at_field}_4i"
-    select start_at.strftime('%M'), from: "#{start_at_field}_5i"
-
-    end_at_field = 'event_end_at'
-    select end_at.strftime('%Y'), from: "#{end_at_field}_1i"
-    select I18n.l(end_at, format: '%B'), from: "#{end_at_field}_2i"
-    select end_at.strftime('%-d'), from: "#{end_at_field}_3i"
-    select end_at.strftime('%H'), from: "#{end_at_field}_4i"
-    select end_at.strftime('%M'), from: "#{end_at_field}_5i"
+    fill_in '内容', with: '宮崎のRubyエンジニアコミュニティ'
 
     click_on '登録する'
     assert_selector 'div.alert', text: '作成しました'
